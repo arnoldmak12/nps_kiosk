de8ad24abefa667a2e30c06f2798129e0af42fc1
arnoldmak12 <arnoldmakarov@yahoo.com>
2019-06-22 15:09:44 -0400

    Updates

---
+++ precache-manifest.ffe4a94d7a8f2f4deef3e14e31a9484c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd4439a969c6c0a2c47803d09f3d9e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84a99d28d73224961f40",
+    "url": "/static/css/main.8df3f492.chunk.css"
+  },
+  {
+    "revision": "3e39cb67a42c455463c9",
+    "url": "/static/js/2.b2cf2467.chunk.js"
+  },
+  {
+    "revision": "84a99d28d73224961f40",
+    "url": "/static/js/main.1e47110f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "31fd68fa5bcb751875f605d4f2e9bbe9",
+    "url": "/static/media/desert.31fd68fa.jpg"
+  },
+  {
+    "revision": "03dd0fcbc0d08314e3a2054e8e7b891a",
+    "url": "/static/media/desert2.03dd0fcb.jpg"
+  },
+  {
+    "revision": "b89785c153b8c6fe9e05024c01a3802e",
+    "url": "/static/media/flowers.b89785c1.jpg"
+  },
+  {
+    "revision": "d87ed5d20c50d8dad6c4d57b4a19476e",
+    "url": "/static/media/grassField.d87ed5d2.jpg"
+  },
+  {
+    "revision": "e7fc24a2b4a8c77b51c7529d17041971",
+    "url": "/static/media/lake.e7fc24a2.jpg"
+  },
+  {
+    "revision": "fc92b65588828e649f966491b460e8c5",
+    "url": "/static/media/largeLogo.fc92b655.png"
+  },
+  {
+    "revision": "863d58960a8feccdc41cc1d13706dbf6",
+    "url": "/static/media/moun2.863d5896.jpg"
+  },
+  {
+    "revision": "cd31d1d17d6928b18fa78f9e60de5e7d",
+    "url": "/static/media/regMoun.cd31d1d1.jpg"
+  },
+  {
+    "revision": "f37767f8b0ee741b0a878b31503c0de3",
+    "url": "/static/media/smallLogo.f37767f8.png"
+  },
+  {
+    "revision": "ba6c2eb37fcb5b094dcf2b31070c7ceb",
+    "url": "/static/media/snowMoun.ba6c2eb3.jpg"
+  }
+]);
