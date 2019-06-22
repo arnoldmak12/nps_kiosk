350328ac0f93888c837e9d3bedd3524cfeb65bec
arnoldmak12 <arnoldmakarov@yahoo.com>
2019-06-22 14:23:09 -0400

    Adjusted Home Screen

--- src/components/Menus/Toolbar/SpecficPages/Home.js
+++ src/components/Menus/Toolbar/SpecficPages/Home.js
@@ -1,35 +1,35 @@
 import React from 'react';
+import './Home.css';
+// const homeStyleHeader = {
+//     position: 'fixed',
+//     zIndex: '10',
+//     top: '20%',
+//     left: '30%',
+//     fontSize: '100px',
+//     color: 'white',
+//     fontFamily: '"Rockwell Extra Bold", "Rockwell Bold", monospace',
+//     fontStyle: 'italic',
+//     fontWeight: '70'
+//   }
 
-const homeStyleHeader = {
-    position: 'fixed',
-    zIndex: '10',
-    top: '20%',
-    left: '30%',
-    fontSize: '170px',
-    color: 'white',
-    fontFamily: '"Rockwell Extra Bold", "Rockwell Bold", monospace',
-    fontStyle: 'italic',
-    fontWeight: '70'
-  }
-
-  const homeStyleSubHeader = {
-    position: 'fixed',
-    zIndex: '10',
-    top: '50%',
-    left: '29%',
-    fontSize: '60px',
-    color: 'white',
-    fontFamily: '"Rockwell Extra Bold", "Rockwell Bold", monospace',
-    fontStyle: 'italic',
-    fontWeight: '70'
-  }
+//   const homeStyleSubHeader = {
+//     position: 'fixed',
+//     zIndex: '10',
+//     top: '50%',
+//     left: '29%',
+//     fontSize: '60px',
+//     color: 'white',
+//     fontFamily: '"Rockwell Extra Bold", "Rockwell Bold", monospace',
+//     fontStyle: 'italic',
+//     fontWeight: '70'
+//   }
 
 const SearchsSpecificToolbar = props => {
     return (
         <div className="home-page">
 
-            <h1 style={homeStyleHeader}>Welcome!</h1>
-            <h2 style={homeStyleSubHeader}>Made By: Arnold Makarov</h2>
+            <h2 className='homeStyleHeader'>National Park Service Information Kiosk</h2>
+            <h3 className='homeStyleSubHeaderName'>Made By: Arnold Makarov</h3>
 
         </div>
     )
